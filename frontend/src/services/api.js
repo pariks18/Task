afb7133d559c0797062bcc495a4d0e24b6267178
Parikshitgole <parikshitgole@Parikshitgoles-MacBook-Air.local>
2025-12-20 04:24:45 +0530

    backend url ag

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-1-wvjx.onrender.com/"
+  baseURL: "https://task-1-wvjx.onrender.com/api"
 });
 
 export default api;
